456f6f18cdfa6f043f1f11dcbc879a290b78d765
Johan Zätterström <johan@zatterstrom.se>
2020-06-22 23:26:15 +0200

    Switch from Vue to React

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import NTyped from 'native-typed'
+
+const App = () => {
+  useEffect(() => {
+    initTyped();
+    console.log('You looking for errors ey? ;)');
+  }, []);
+
+  const connect = [
+    {
+      name: 'LinkedIn',
+      url: 'https://se.linkedin.com/in/zatterstrom',
+      icon: 'fab fa-linkedin',
+    },
+    { name: 'GitHub',
+      url: 'https://github.com/jzatt',
+      icon: 'fab fa-github-square',
+    },
+  ];
+
+  const initTyped = () => {
+    new NTyped(document.querySelector('#typed'), {
+      strings: [
+        'Frontend',
+        '*.{js, ts}',
+        'React, Vue, WordPress',
+        '*.{css, scss}',
+        '*.{sketch, psd, ai, indd}',
+      ],
+      typeSpeed: 50,
+      backDelay: 1000,
+      deleteSpeed: 15,
+      classes: {
+        cursor: 'typed-cursor'
+      }
+    });
+  };
+  
+  return (
+    <main className="container">
+      <div className="scene">
+        <div className="scene-text">
+          <h1 className="title">
+            Johan Zätterström
+          </h1>
+          <h3 className="subtitle">
+            <span id="typed" />
+          </h3>
+        </div>
+      </div>
+      <div className="footer">
+        {connect.map((social) => (
+          <a
+            key={social.name}
+            title={social.name}
+            href={social.url}
+          >
+            <i className={social.icon} />
+          </a>
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/scss/app.scss';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
